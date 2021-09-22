cbf3b702b889e5ccbd089393d5c249348e673533
Rikish <rikish.kamboj@student.uts.edu.au>
2021-09-22 20:46:48 +1000

    Basic draft for FunctionsNames

---
+++ FunctionsNames/FunctionsNames.cpp
@@ -0,0 +1,45 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+using namespace llvm;
+
+namespace {
+  struct FunctionsNamesPass : public FunctionPass {
+    static char ID;
+    FunctionsNamesPass() : FunctionPass(ID) {}
+
+    virtual bool runOnFunction(Function &F) {
+      errs() << "Found Function: " << F.getName() << "!\n";
+      
+      for (auto &B : F) {
+        for (auto &I : B) {
+          errs() << "Found Instruction: " << I.getOpcodeName() << "!\n";
+        }
+      }
+
+
+      return false;
+
+
+      
+
+
+
+
+    }
+  };
+}
+
+char FunctionsNamesPass::ID = 0;
+
+// Automatically enable the pass.
+// http://adriansampson.net/blog/clangpass.html
+static void registerFunctionsNamesPass(const PassManagerBuilder &,
+                         legacy::PassManagerBase &PM) {
+  PM.add(new FunctionsNamesPass());
+}
+static RegisterStandardPasses
+  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
+                 registerFunctionsNamesPass);
---
+++ a.c
@@ -0,0 +1,10 @@
+int main(){
+    int x = 56;
+    int y = 73;
+    int z = x + y;
+    return z;
+}
+
+int cap(){
+    return 69;
+}
