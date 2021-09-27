ea34df4d0882b87d401c841fa6186423b5413c8c
DaryllRicalde <rstevendaryll@gmail.com>
2021-09-27 16:07:58 +1000

    working version of demangler

--- DemanglerPass/DemanglerPass.cpp
+++ DemanglerPass/DemanglerPass.cpp
@@ -38,7 +38,7 @@ namespace {
             StringRef name = F.getName();
             // name.str() -> get contents of StringRef object as a string
 
-            if(name.str().rfind("_",0) == 0){
+            if(!name.str().empty()){
                 errs() << "Mangled name: " << name << "\n";
                 std::string demangled_name = demangle(name.str());
                 if(demangled_name == "") { errs() << "Empty demangled name returned!" << "\n"; }
@@ -50,13 +50,6 @@ namespace {
         }
 
         std::string demangle(const std::string &name){
-            
-            // Create instance of Demangler
-            //char *Buf = static_cast<char *>(std::malloc(size));
-            //ItaniumPartialDemangler Mangler;
-
-            //char* result = Mangler.getFunctionBaseName(Buf, &size);
-            //llvm::outs() << "Result: " << result << "\n";
 
             int status = -1;        // some arbitrary value to eliminate the compiler warning
 
@@ -68,7 +61,9 @@ namespace {
             size_t size = 1024;
             if(name.c_str() != nullptr){
                 char* result = abi::__cxa_demangle(name.c_str(),buf,&size,&status);
-                return result;
+                if(result != nullptr){
+                    return result;
+                }
             }
             
             return "";
